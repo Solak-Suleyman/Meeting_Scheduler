7e3572af00d8adedc0d3b6a0634c8e29a79fd87d
suleyman-solk <slymn.solk.01@gmail.com>
2023-09-06 14:15:28 +0300

    adding authorization

---
+++ WebApplication4/Consts.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication4
+{
+    public class Consts
+    {
+        public const string UserName = "sylymnsolk22_";
+        public const string Password = "Mypass1*";
+    }
+}
---
+++ WebApplication4/Models/Context/AuthContex.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models.Context
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext(DbContextOptions<AuthContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+
+}
